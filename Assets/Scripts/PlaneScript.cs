5540d5529910b2e7cb9f36dce79ec6e41e545464
Austin Clarke <aclarke822@gmail.com>
2016-11-27 22:14:06 -0600

    Procedural Plane Signed-off-by: Austin Clarke <aclarke822@gmail.com>

---
+++ Assets/Scripts/PlaneScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneScript : MonoBehaviour {
+
+	private float gridSize = 10.0f;
+	private Vector3 gridPosition = new Vector3(0,0,0) ;
+	private Vector3 currentPlayerPosition;
+	private Plane[] planes;
+	public Rigidbody player;
+
+	void Awake() {
+		currentPlayerPosition = player.transform.position;
+
+		CreatePlane (gridSize * gridPosition.x + gridSize, 0.0f, gridSize * gridPosition.z + gridSize);
+		//CreatePlane (gridSize*gridPosition.x, 0.0f, gridSize*gridPosition.z);
+		//CreatePlane (gridSize*gridPosition.x, 0.0f, gridSize*gridPosition.z);
+		//CreatePlane (gridSize*gridPosition.x, 0.0f, gridSize*gridPosition.z);
+		//CreatePlane (gridSize*gridPosition.x, 0.0f, gridSize*gridPosition.z);
+		//CreatePlane (gridSize*gridPosition.x, 0.0f, gridSize*gridPosition.z);
+		//CreatePlane (gridSize*gridPosition.x, 0.0f, gridSize*gridPosition.z);
+		//CreatePlane (gridSize*gridPosition.x, 0.0f, gridSize*gridPosition.z);
+		//CreatePlane (gridSize*gridPosition.x, 0.0f, gridSize*gridPosition.z);
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentPlayerPosition = player.transform.position;
+		
+	}
+
+	public Mesh CreateMesh(float width, float height, float length)
+	{
+		Mesh m = new Mesh();
+		m.name = "ScriptedMesh";
+		m.vertices = new Vector3[] {
+			new Vector3(-width, height, -length),
+			new Vector3(-width, height, length),
+			new Vector3(width, height, length),
+			new Vector3(width, height, -length)
+		};
+		m.uv = new Vector2[] {
+			new Vector2 (0, 0),
+			new Vector2 (0, 1),
+			new Vector2(1, 1),
+			new Vector2 (1, 0)
+		};
+		m.triangles = new int[] { 0, 1, 2, 0, 2, 3};
+		m.RecalculateNormals();
+
+		return m;
+	}
+
+	public GameObject CreatePlane (float width, float height, float length)
+	{
+		GameObject plane = new GameObject("Plane");
+
+		Mesh mesh = CreateMesh(width, height, length);
+		MeshFilter meshFilter = (MeshFilter)plane.AddComponent(typeof(MeshFilter));
+		MeshCollider meshCollider = (MeshCollider)plane.AddComponent (typeof(MeshCollider));
+		MeshRenderer meshRenderer = plane.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+
+		meshFilter.mesh = mesh;
+		meshCollider.sharedMesh = mesh;
+		meshRenderer.material.shader = Shader.Find ("Particles/Additive");
+
+		Texture2D tex = new Texture2D(1, 1);
+		tex.SetPixel(0, 0, Color.red);
+		tex.Apply();
+
+		meshRenderer.material.mainTexture = tex;
+		meshRenderer.material.color = Color.red;
+		return plane;
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody player;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		player = GetComponent<Rigidbody> ();
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		player.AddForce (movement * speed);
+		
+	}
+
+	public Vector3 GetPlayerPosition ()
+	{
+		
+		return player.transform.position;
+	}
+}
